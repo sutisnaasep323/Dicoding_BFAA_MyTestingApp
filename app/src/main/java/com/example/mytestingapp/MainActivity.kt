72bdc0f4b82450c2ed2082fe61da7550d6dc243f
Asep Sutisna <sutisnaasep323@gmail.com>
2021-08-14 22:59:37 +0700

    Dicoding_BFAA_MyTestingApp_Error 2_IndexOutOfBoundsException"

--- app/src/main/java/com/example/mytestingapp/MainActivity.kt
+++ app/src/main/java/com/example/mytestingapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.mytestingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -10,20 +11,31 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var btnSetValue: Button
     private lateinit var tvText: TextView
+
+    private var names = ArrayList<String>()
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         
         tvText = findViewById(R.id.tv_text)
-        btnSetValue = findViewById(R.id.btn_set_value)
+        btnSetValue = findViewById(R.id.btn_set_value) // awalnya tidak ada inisialisasi (tidak ada kode baris ini) sehingga errornya NullPointerException
 
         btnSetValue.setOnClickListener(this)
+
+        names.add("Narenda Wicaksono")
+        names.add("Kevin")
+        names.add("Yoza")
     }
 
     override fun onClick(view: View) {
         if ( view.id == R.id.btn_set_value){
-            tvText.text = "19"
+            Log.d("MainActivity", names.toString())
+            val name = StringBuilder()
+            for (i in 0..2) { // awalnya 3 sehingga errornya IndexOutOfBoundsException
+                name.append(names[i]).append("\n")
+            }
+            tvText.text = name.toString()
         }
     }
 }
